38d40702cd90884a1909b6eb9344760acb2cefaf
Sergey Kalinovsky <lord.skiminok@gmail.com>
2017-08-29 12:02:34 +0300

    Refactor icon id calculating

--- app/src/main/java/com/revotechs/weather/MainActivity.java
+++ app/src/main/java/com/revotechs/weather/MainActivity.java
@@ -1,6 +1,5 @@
 package com.revotechs.weather;
 
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
@@ -69,64 +68,8 @@ public class MainActivity extends AppCompatActivity {
                 windSpeedText.append(" m/s");
 
                 ImageView weatherImage = (ImageView) findViewById(R.id.weather_image);
-                Drawable icon = getDrawable(R.drawable.na);
-                switch (forecast.getWeather().get(0).getIcon()) {
-                    case "01d":
-                        icon = getDrawable(R.drawable.image01d);
-                        break;
-                    case "01n":
-                        icon = getDrawable(R.drawable.image01n);
-                        break;
-                    case "02d":
-                        icon = getDrawable(R.drawable.image02d);
-                        break;
-                    case "02n":
-                        icon = getDrawable(R.drawable.image02n);
-                        break;
-                    case "03d":
-                        icon = getDrawable(R.drawable.image03d);
-                        break;
-                    case "03n":
-                        icon = getDrawable(R.drawable.image03n);
-                        break;
-                    case "04d":
-                        icon = getDrawable(R.drawable.image04d);
-                        break;
-                    case "04n":
-                        icon = getDrawable(R.drawable.image04n);
-                        break;
-                    case "09d":
-                        icon = getDrawable(R.drawable.image09d);
-                        break;
-                    case "09n":
-                        icon = getDrawable(R.drawable.image09n);
-                        break;
-                    case "10d":
-                        icon = getDrawable(R.drawable.image10d);
-                        break;
-                    case "10n":
-                        icon = getDrawable(R.drawable.image10n);
-                        break;
-                    case "11d":
-                        icon = getDrawable(R.drawable.image11d);
-                        break;
-                    case "11n":
-                        icon = getDrawable(R.drawable.image11n);
-                        break;
-                    case "13d":
-                        icon = getDrawable(R.drawable.image13d);
-                        break;
-                    case "13n":
-                        icon = getDrawable(R.drawable.image13n);
-                        break;
-                    case "50d":
-                        icon = getDrawable(R.drawable.image50d);
-                        break;
-                    case "50n":
-                        icon = getDrawable(R.drawable.image50n);
-                        break;
-                }
-                weatherImage.setImageDrawable(icon);
+                int iconId = getResources().getIdentifier("image" + forecast.getWeather().get(0).getIcon(), "drawable", getPackageName());
+                weatherImage.setImageDrawable(getDrawable(iconId));
             }
 
             @Override
